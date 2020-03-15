bc9c7a410a8cc8abab6bddeb42fae0702a5c6b49
s.murakami <himegreat0517@gmail.com>
2020-03-15 10:44:19 +0900

    ビューコントローラー作成 トップページのレイアウト途中まで作成。

---
+++ muzeumguide/resources/views/layout/muzeumguide.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="岡山県の美術館・博物館を紹介しているサイトです。">
+        <title>@yield('title')</title>
+        <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+        <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    </head>
+    <body>
+        <div class="whole-wrapper">
+            <header class="page-header wrapper">
+                <div class="header-navi">
+                    <input class="search" type="search" name="" value="検索" maxlength="20">
+                    <div class="login-logout">
+                        <a href="#">ログイン</a>
+                        <a href="#">ログアウト</a>
+                    </div>
+                </div>
+                <h1 class="header-title">岡山県のミュージアムガイド</h1>
+                <nav>
+                    <ul class="category-navi">
+                        <li><a class="category home" href="/muzeumguide">ホーム</a></li>
+                        <li><a class="category art" href="/#">美術</a></li>
+                        <li><a class="category history" href="/#">歴史</a></li>
+                        <li><a class="category nature" href="/#">自然</a></li>
+                        <li><a class="category sience" href="/#">科学</a></li>
+                    </ul>
+                </nav>
+            </header>
+
+            <main>
+                <div class="contents wrapper">
+                  @yield('contents')
+                </div>
+            </main>
+
+            <footer>
+                <div class="wrapper">
+                    <p><small>&copy; 2020 muzeum Guide Of Okayama Prefecture.</p>
+                </div>
+            </footer>
+        </div>
+    </body>
+</html>
---
+++ muzeumguide/resources/views/muzeumguide/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layout.muzeumguide')
+
+@section('title', '岡山県のミュージアムガイド')
+
+@section('contents')
+    <div class="contents">
+        <div class="art">
+          <a href="#">美術</a>
+        </div>
+        <div class="art">
+          <a href="#">歴史</a>
+        </div>
+        <div class="art">
+          <a href="#">自然</a>
+        </div>
+        <div class="art">
+          <a href="#">科学</a>
+        </div>
+    </div>
+@endsection
