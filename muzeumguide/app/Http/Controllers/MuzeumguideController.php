b1deb0c6ecc8a080195f6992f5722e19d8fbd1a1
s.murakami <himegreat0517@gmail.com>
2020-03-16 21:51:13 +0900

    カテゴリーを表示するためにコントローラーを修正。

--- muzeumguide/app/Http/Controllers/MuzeumguideController.php
+++ muzeumguide/app/Http/Controllers/MuzeumguideController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Category;
 
 class MuzeumguideController extends Controller
 {
+
+    private $category;
+
+    public function __construct(Category $category)
+    {
+        $this->category = $category;
+    }
+
     public function index(Request $request)
     {
-        return view('muzeumguide.index');
+        $categories = $this->category->all();
+        return view('muzeumguide.index', ['categories' => $categories]);
     }
 }
