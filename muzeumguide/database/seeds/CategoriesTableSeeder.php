4a60b57a9878a1eb033db170098cb625bbc08b64
s.murakami <himegreat0517@gmail.com>
2020-03-16 21:49:40 +0900

    ・カテゴリーテーブルを作成した。 ・ダミーテーブルも作成し、シーダーを実行した。 ・divタグのクラス名を付けるために、name_enというテーブルを作成。

---
+++ muzeumguide/database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categories')->insert([
+            'name' => '美術',
+            'description' => '大原美術館など有名な美術館などが多数あります。',
+            'name_en' => 'art',
+        ]);
+
+        DB::table('categories')->insert([
+            'name' => '歴史',
+            'description' => '大原美術館など有名な美術館などが多数あります。',
+            'name_en' => 'history',
+        ]);
+
+        DB::table('categories')->insert([
+            'name' => '自然',
+            'description' => '大原美術館など有名な美術館などが多数あります。',
+            'name_en' => 'nature',
+        ]);
+
+        DB::table('categories')->insert([
+            'name' => '科学',
+            'description' => '大原美術館など有名な美術館などが多数あります。',
+            'name_en' => 'science',
+        ]);
+    }
+}
