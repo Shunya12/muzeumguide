4fa1f35645544ae49163128db655134356caf8bd
s.murakami <himegreat0517@gmail.com>
2020-03-16 21:44:30 +0900

    laravelにもともとあったトップページだが、必要ないので削除した。

--- muzeumguide/routes/web.php
+++ muzeumguide/routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('muzeumguide', 'MuzeumguideController@index');
+
+Route::get('/', 'MuzeumguideController@index');
