0a9ba28469ec6663fe71761f4070953695449239
s.murakami <himegreat0517@gmail.com>
2020-03-22 10:08:41 +0900

    データベースの不具合修正のため

---
+++ muzeumguide/resources/views/muzeumguide/category_history.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.muzeumguide')
+
+@section('title', '岡山県のミュージアムガイド/歴史')
+
+@section('description', '岡山県の美術館・博物館を紹介しているサイトです。')
+
+@section('content')
+
+@endsection
---
+++ muzeumguide/resources/views/muzeumguide/category_science.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.muzeumguide')
+
+@section('title', '岡山県のミュージアムガイド/科学')
+
+@section('description', '岡山県の美術館・博物館を紹介しているサイトです。')
+
+@section('content')
+
+@endsection
