621a49c23da3b6f408d5d96e81c9a7c29a15843b
s.murakami <himegreat0517@gmail.com>
2020-03-22 10:00:02 +0900

    各カテゴリービューを表示できるように修正

--- muzeumguide/app/Http/Controllers/MuzeumguideController.php
+++ muzeumguide/app/Http/Controllers/MuzeumguideController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Category;
+use Illuminate\Support\Facades\DB;
 
 class MuzeumguideController extends Controller
 {
@@ -17,7 +18,28 @@ class MuzeumguideController extends Controller
 
     public function index(Request $request)
     {
-        $categories = $this->category->all();
+        // $categories = $this->category->all();
+        $categories = DB::select('select * from categories');
         return view('muzeumguide.index', ['categories' => $categories]);
+    }
+
+    public function category_art(Request $request)
+    {
+        return view('muzeumguide.category_art');
+    }
+
+    public function category_history(Request $request)
+    {
+        return view('muzeumguide.category_history');
+    }
+
+    public function category_nature(Request $request)
+    {
+        return view('muzeumguide.category_nature');
+    }
+
+    public function category_science(Request $request)
+    {
+        return view('muzeumguide.category_science');
     }
 }
