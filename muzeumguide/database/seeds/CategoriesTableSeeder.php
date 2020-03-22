3962651104909f7d522b143f3c35f4a45b947f9d
s.murakami <himegreat0517@gmail.com>
2020-03-22 10:00:20 +0900

    urlテーブルを作成

--- muzeumguide/database/seeds/CategoriesTableSeeder.php
+++ muzeumguide/database/seeds/CategoriesTableSeeder.php
@@ -18,24 +18,28 @@ class CategoriesTableSeeder extends Seeder
             'name' => '美術',
             'description' => '大原美術館など有名な美術館などが多数あります。',
             'name_en' => 'art',
+            'url' => 'muzeumguide/art',
         ]);
 
         DB::table('categories')->insert([
             'name' => '歴史',
             'description' => '大原美術館など有名な美術館などが多数あります。',
             'name_en' => 'history',
+            'url' => 'muzeumguide/history',
         ]);
 
         DB::table('categories')->insert([
             'name' => '自然',
             'description' => '大原美術館など有名な美術館などが多数あります。',
             'name_en' => 'nature',
+            'url' => 'muzeumguide/nature',
         ]);
 
         DB::table('categories')->insert([
             'name' => '科学',
             'description' => '大原美術館など有名な美術館などが多数あります。',
             'name_en' => 'science',
+            'url' => 'muzeumguide/science',
         ]);
     }
 }
