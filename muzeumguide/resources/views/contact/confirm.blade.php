2dfae68c0a3f83a3a2a7c34c7bbd0ba52c7c255a
s.murakami <himegreat0517@gmail.com>
2020-04-16 13:39:52 +0900

    お問い合わせページの以下のテンプレートを作成 ・お問い合わせページTOP ・確認ページ

--- muzeumguide/resources/views/contact/confirm.blade.php
+++ muzeumguide/resources/views/contact/confirm.blade.php
@@ -3,5 +3,37 @@
 @section('title', '確認ページ')
 
 @section('contents')
+@if (count($errors) > 0)
+    <ul>
+        @foreach ($errors as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+@endif
 
+    <div class="wrapper">
+        <div class="container">
+            <h2 class="confirm-title">お問い合わせ内容の確認</h2>
+            <p>{{ $confirm_message }}</p>
+        </div>
+
+        {{ Form::open(['route' => 'contact.thanks']) }}
+            <div class="container1">
+                <label for="name">お名前</label>
+                {{ Form::text('name', $inputs['name'], ['class' => 'form_name', 'readonly']) }}
+                <label for="email">メールアドレス</label>
+                {{ Form::email('email', $inputs['email'], ['class' => 'form_email', 'readonly']) }}
+                <label for="message">メッセージ</label>
+                {{ Form::textarea('message', $inputs['message'], ['class' => 'form_message', 'readonly']) }}
+            </div>
+            <div class="container2">
+                <div class="submit">
+                    {{ Form::submit('送信', ['name' => 'action', 'class' => 'btn']) }}
+                </div>
+                <div class="correct">
+                    {{ Form::submit('入力し直す', ['name' => 'action', 'class' => 'btn']) }}
+                </div>
+            </div>
+        {{ Form::close() }}
+    </div>
 @endsection
--- muzeumguide/resources/views/contact/contact.blade.php
+++ muzeumguide/resources/views/contact/contact.blade.php
@@ -5,25 +5,35 @@
 @section('description', '何かご要望がございましたら、お気軽にお問い合わせください。')
 
 @section('contents')
-    <h2 class="contact-title">お問い合わせ</h2>
-    {{ Form::open(['route' => 'contact.confirm', 'method' => 'POST']) }}
-        <div>
-            <label for="name">お名前</label>
-            {{ Form::text('name', null, ['id' => 'name']) }}
-            <input type="text" id="name" name="name">
-        </div>
 
-        <div>
-            <label for="email">メールアドレス</label>
-            {{ Form::email('email', null, ['id' => 'email']) }}
-            <input type="email" id="email" name="email">
-        </div>
+@if (count($errors) > 0)
+    <ul>
+        @foreach ($errors as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+@endif
 
-        <div>
-            <label for="message">メッセージ</label>
-            {{ Form::textarea('message', null, ['id' => 'message']) }}
-            <textarea id="message" name="message"></textarea>
+    <div class="container">
+        <h2 class="contact-title">お問い合わせ</h2>
+        {{ Form::open(['route' => 'contact.confirm', 'method' => 'POST']) }}
+        <div class="container">
+            <div>
+                <label for="name">お名前</label>
+                {{ Form::text('name', null, ['id' => 'name']) }}
+            </div>
+
+            <div>
+                <label for="email">メールアドレス</label>
+                {{ Form::email('email', null, ['id' => 'email']) }}
+            </div>
+
+            <div>
+                <label for="message">メッセージ</label>
+                {{ Form::textarea('message', null, ['id' => 'message', 'rows' => '5']) }}
+            </div>
+            {{ Form::submit('確認画面へ', ['class' => 'btn']) }}
+        {{ Form::close() }}
         </div>
-        {{ Form::submit('確認画面へ') }}
-    {{ Form::close() }}
+    </div>
 @endsection
