7cd0c2b86dbaa6cfec589cf184b64906c0102a9a
松島一徳 <>
2020-05-03 12:04:22 +0900

    カードレイアウトを作成

--- muzeumguide/resources/views/muzeumguide/index.blade.php
+++ muzeumguide/resources/views/muzeumguide/index.blade.php
@@ -5,12 +5,16 @@
 @section('description', '岡山県の美術館・博物館を紹介しているサイトです。')
 
 @section('contents')
-    <div class="contents">
+    <div class="contents card-group">
         @foreach($categories as $category)
-        <div class="category {{$category->name_en}}">
-          <a href="{{ route('show.category', ['category' => $category->name_en]) }}">{{$category->name}}</a>
-          <p>{{ $category->description }}</p>
-        </div>
+          <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="{{ asset('img/' . $category->name_en . '_thumb.jpg' ) }}" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">{{ $category->name_en }}</h5>
+              <p class="card-text">{{ $category->description }}</p>
+              <a href="{{ route('show.category', ['category' => $category->name_en]) }}" class="btn btn-primary">{{ $category->name }}</a>
+            </div>
+          </div>
         @endforeach
     </div>
 @endsection
